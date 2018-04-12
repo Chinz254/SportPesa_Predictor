cd4077611fe4c1e2484ba62864eeb0f8a051b3ba
Chinz254 <19781386+Chinz254@users.noreply.github.com>
2018-04-12 14:55:06 +0300

    First Upload
    This is just the beginning......
    Be sure I'll keep updating.

---
+++ sportpesa_predictor.py
@@ -0,0 +1,25 @@
+import random
+def bounty_hunter():
+    n = 0
+    while n <= 12:
+        def game_predictor():
+            rng = random.Random()
+            number = rng.randrange(1,4)
+            if number == 1:
+                return "X"
+            elif number == 2:
+                return "1"
+            else:
+                return "2"
+
+        predicted_no = game_predictor()
+
+        print(predicted_no, end= " ")
+        n += 1
+    jp = str(bounty_hunter())
+    jackpt = ''.join(str(x) for x in [jp])
+
+
+
+type(jackpt)
+print(jackpt[3:6])
